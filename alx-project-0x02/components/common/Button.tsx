0a6896f5f74b5676c8c4ab966c05bc23b284f0e8
DevMandate <wafulacaleb233@gmail.com>
2025-07-26 11:48:49 +0300

    Fix: Button component import and ButtonProps type usage

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ButtonProps } from '../../interfaces';
+import { type ButtonProps } from '@/interfaces';
 
 export const Button: React.FC<ButtonProps> = ({ size, shape, label, onClick }) => {
   const sizeClasses = {
